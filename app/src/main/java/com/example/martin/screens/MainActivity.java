9af63e9386298db050e7a32c6404665910b1c018
MartinGalvan97 <martingalvancovarrubias@gmail.com>
2017-06-13 22:35:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/martin/screens/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.martin.screens;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ingresar(View view)
+    {
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
+
+}
