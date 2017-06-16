90ea74d67ba16bcd3bb3217cb5776a31753e385c
joanluarloko1 <angel_loko_jose@hotmail.com>
2017-06-15 22:28:41 -0700

    kc v lsmc

--- app/src/main/java/com/example/martin/screens/MainActivity.java
+++ app/src/main/java/com/example/martin/screens/MainActivity.java
@@ -26,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this,Main2Activity.class);
         startActivity(intent);
     }
+    public void ingresardostres(View view)
+    {
+        Intent intent = new Intent(this,Main2Activity.class);
+        startActivity(intent);
+    }
 
 
 }
